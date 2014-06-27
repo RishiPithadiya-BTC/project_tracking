75d3ab3135a86a879a9b3ba0f4899798ca9f628d
RishiPithadiya-BTC <rishi.pithadiya@beaverlogic.com>
2014-06-27 16:13:31 +0530

    After adding Project Controller

---
+++ app/views/projects/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @project, :id, :name, :code, :description, :billing_type, :start_date, :deadline_date, :end_date, :github_url, :status, :created_at, :updated_at
---
+++ db/migrate/20140627102846_create_projects.rb
@@ -0,0 +1,17 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.string :code
+      t.string :description
+      t.string :billing_type
+      t.datetime :start_date
+      t.datetime :deadline_date
+      t.datetime :end_date
+      t.string :github_url
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
